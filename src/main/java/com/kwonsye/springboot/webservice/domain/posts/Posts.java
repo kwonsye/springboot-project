763a10cf80f2cc70cda4a510f516e982ff1d93f2
kwonsye <kwonsye@naver.com>
2020-03-06 22:42:13 +0900

    - update api    - controller 수정    - service 수정    - PostsUpdateRequestDto - findById api    - controller 수정    - service 수정    - PostsResponseDto

--- src/main/java/com/kwonsye/springboot/webservice/domain/posts/Posts.java
+++ src/main/java/com/kwonsye/springboot/webservice/domain/posts/Posts.java
@@ -28,4 +28,9 @@ public class Posts { //DB와 맞닿는 핵심클래스
         this.content = content;
         this.author = author;
     }
+
+    public void update(String title, String content){
+        this.title = title;
+        this.content = content;
+    }
 }
--- src/main/java/com/kwonsye/springboot/webservice/service/posts/PostsService.java
+++ src/main/java/com/kwonsye/springboot/webservice/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package com.kwonsye.springboot.webservice.service.posts;
 
+import com.kwonsye.springboot.webservice.domain.posts.Posts;
 import com.kwonsye.springboot.webservice.domain.posts.PostsRepository;
+import com.kwonsye.springboot.webservice.web.dto.PostsResponseDto;
 import com.kwonsye.springboot.webservice.web.dto.PostsSaveRequestDto;
+import com.kwonsye.springboot.webservice.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -15,5 +18,22 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto){
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto){
+        Posts post = postsRepository.findById(id)
+                .orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+
+        //update 쿼리를 날리는 부분이 없음, JPA의 영속성 컨텍스트(엔티티를 영구 저장하는 환경) 때문임
+        post.update(requestDto.getTitle(), requestDto.getContent());
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id){
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+
+        return new PostsResponseDto(entity);
     }
 }
