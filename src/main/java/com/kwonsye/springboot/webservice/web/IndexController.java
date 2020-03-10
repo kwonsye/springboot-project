94d46a01228108115092159908aadf089822fa99
kwonsye <kwonsye@naver.com>
2020-03-10 22:41:56 +0900

    add delete api and update mapping in IndexController

--- src/main/java/com/kwonsye/springboot/webservice/web/IndexController.java
+++ src/main/java/com/kwonsye/springboot/webservice/web/IndexController.java
@@ -1,10 +1,12 @@
 package com.kwonsye.springboot.webservice.web;
 
 import com.kwonsye.springboot.webservice.service.posts.PostsService;
+import com.kwonsye.springboot.webservice.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -21,5 +23,13 @@ public class IndexController { //mustache url 매핑
     @GetMapping("/posts/save")
     public String postsSave(){
         return "posts-save"; //posts-save.mustache 페이지 매핑
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model){
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update"; //posts-update.mustache 페이지 매핑
     }
 }
